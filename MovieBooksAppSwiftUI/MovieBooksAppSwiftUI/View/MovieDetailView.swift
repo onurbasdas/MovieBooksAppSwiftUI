4cee6a88e74eba3d0214b4126f77d29c77190efd
onur.basdas <onurebasdase@hotmail.com>
2023-03-05 16:33:16 +0300

    Detail view completed.

--- MovieBooksAppSwiftUI/MovieBooksAppSwiftUI/View/MovieDetailView.swift
+++ MovieBooksAppSwiftUI/MovieBooksAppSwiftUI/View/MovieDetailView.swift
@@ -13,11 +13,27 @@ struct MovieDetailView: View {
     
     @ObservedObject var movieDetailViewModel = MovieDetailViewModel()
     
+    var uiScreen = UIScreen.main.bounds
+     
+    
     var body: some View {
         VStack {
-            Text(movieDetailViewModel.movieDetails?.plot ?? "").onAppear {
-                self.movieDetailViewModel.getMovieDetail(imdbId: imdbId)
+            
+            let movieDetailCommon = movieDetailViewModel.movieDetails
+            
+            SpecialImage(url: movieDetailCommon?.poster ?? "").frame(width: uiScreen.width * 0.6, height: uiScreen.height * 0.3, alignment: .center)
+            
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(movieDetailCommon?.title ?? "Shows movie title.").padding().font(.title).bold()
+                Text(movieDetailCommon?.plot ?? "Shows movie plot.").padding()
+                Text("Yönetmen: \(movieDetailCommon?.director ?? "Shows director.")").padding()
+                Text("Yazar: \(movieDetailCommon?.writer ?? "Shows writer.")").padding()
+                Text("Ödüller: \(movieDetailCommon?.awards ?? "Shows awards.")").padding()
+                Text("Yıl: \(movieDetailCommon?.year ?? "Shows year.")").padding()
             }
+        }.onAppear {
+            self.movieDetailViewModel.getMovieDetail(imdbId: imdbId)
         }
     }
 }
